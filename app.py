8732a7849297e1ab9873e8ee0a6aa183624309bf
micxba <mb@baumannlive.com>
2025-06-26 13:56:23 -0400

    Support multiple branches and add selector

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from utils.parser import fetch_github_yaml_files, extract_apps
 from utils.helm import fetch_all_chart_versions
+from config import GIT_BRANCHES
 import asyncio
 
 app = Flask(__name__)
@@ -8,18 +9,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Initial data without remote helm version checks
-    docs = asyncio.run(fetch_github_yaml_files())
+    default_branch = GIT_BRANCHES[0]
+    docs = asyncio.run(fetch_github_yaml_files(default_branch))
     apps = extract_apps(docs)
     for app in apps:
         app['latestVersion'] = "loading..."
-    return render_template("index.html", apps=apps)
+    return render_template(
+        "index.html",
+        apps=apps,
+        branches=GIT_BRANCHES,
+        selected_branch=default_branch,
+    )
 
-from utils.helm import fetch_all_chart_versions
+@app.route('/data')
+def data():
+    branch = request.args.get('branch', GIT_BRANCHES[0])
+    docs = asyncio.run(fetch_github_yaml_files(branch))
+    apps = extract_apps(docs)
+    for app_data in apps:
+        app_data['latestVersion'] = "loading..."
+    return jsonify(apps)
 
 @app.route('/latest')
 def latest():
     async def load_latest():
-        docs = await fetch_github_yaml_files()
+        branch = request.args.get('branch', GIT_BRANCHES[0])
+        docs = await fetch_github_yaml_files(branch)
         apps = extract_apps(docs)
         tasks = [fetch_all_chart_versions(app['repoURL'], app['chart']) for app in apps]
         all_versions_list = await asyncio.gather(*tasks)
